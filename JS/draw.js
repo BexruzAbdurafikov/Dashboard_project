3f9af4c11c3bebc0ca9685da6d6aa4e206f24d51
BexruzAbdurafikov <abdurafikovbexruz123@gmail.com>
2025-02-21 13:25:51 +0500

    edited

--- JS/draw.js
+++ JS/draw.js
@@ -5,6 +5,9 @@ const divs = document.querySelector('.divs');
 function drawTable(arr) {
     tableBody.innerHTML = ''
     for (let item of arr) {
+        const actions = document.createElement('td');
+        const delet = document.createElement('div');
+        const edit = document.createElement('div');
         const title = document.createElement('td');
         const desc = document.createElement('td');
         const date = document.createElement('td');
@@ -12,6 +15,12 @@ function drawTable(arr) {
         const isDone = document.createElement('td');
         const tr = document.createElement('tr');
 
+        delet.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="" viewBox="0 0 24 24">
+<path d="M 10 2 L 9 3 L 4 3 L 4 5 L 5 5 L 5 20 C 5 20.522222 5.1913289 21.05461 5.5683594 21.431641 C 5.9453899 21.808671 6.4777778 22 7 22 L 17 22 C 17.522222 22 18.05461 21.808671 18.431641 21.431641 C 18.808671 21.05461 19 20.522222 19 20 L 19 5 L 20 5 L 20 3 L 15 3 L 14 2 L 10 2 z M 7 5 L 17 5 L 17 20 L 7 20 L 7 5 z M 9 7 L 9 18 L 11 18 L 11 7 L 9 7 z M 13 7 L 13 18 L 15 18 L 15 7 L 13 7 z"></path>
+</svg>`;
+        edit.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="" viewBox="0 0 24 24">
+    <path d="M 18 2 L 15.585938 4.4140625 L 19.585938 8.4140625 L 22 6 L 18 2 z M 14.076172 5.9238281 L 3 17 L 3 21 L 7 21 L 18.076172 9.9238281 L 14.076172 5.9238281 z"></path>
+</svg>`;
         title.textContent = item.title;
         desc.textContent = item.desc;
         date.textContent = item.date;
@@ -25,8 +34,10 @@ function drawTable(arr) {
         } else if (item.isDone === 'Не выполнено') {
             isDone.classList.add('not-done');
         }
+        actions.classList.add('actions');
 
-        tr.append(title, desc, date, time, isDone);
+        actions.append(delet, edit);
+        tr.append(title, desc, date, time, isDone, actions);
         tableBody.append(tr);
     }
     table.append(tableBody);
